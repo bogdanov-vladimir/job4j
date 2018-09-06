0d60a51af9fdfb0bc970421f71c8d8373f1fadbd
bogdanov-vladimir <bogd_vlad@mail.ru>
2018-09-06 20:43:56 +0600

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
-
+/**
+*Вывод строки "Hello word" в консоль
+*@author Bogdanov Vladimir (bogd_vlad@mail.ru)
+*@version 1.0
+*@param args - аргументы
+*@return void
+*@since 06.09.2018
+*/
 class Calculate{
 	public static void main(String args[]){
 		System.out.println("Hello word!");

2556c1fa83127d39a114bd65414cc5c737f48b93
bogdanov-vladimir <bogd_vlad@mail.ru>
2019-05-12 14:21:27 +0600

    6.8. Удаление дубликатов в массиве (исправления).

--- chapter_001/src/main/java/ru/job4j/array/ArrayDublicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDublicate.java
@@ -8,11 +8,11 @@ public class ArrayDublicate {
         int idxLastRemove = array.length - 1;
         
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < idxLastRemove; j++) {
+            for (int j = i + 1; j < idxLastRemove; j++) {
                 
-                if (i == j) {
+                /*if (i == j) {
                     continue;
-                }                
+                } */               
                 
                 if (array[i].equals(array[j])) {
                     String dublicateItem = array[j];

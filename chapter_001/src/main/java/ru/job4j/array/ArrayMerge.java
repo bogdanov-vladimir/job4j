e7f1bfd08e4d7264039426af57a0508be2fb45fd
bogdanov-vladimir <bogd_vlad@mail.ru>
2019-10-09 23:29:57 +0600

    1. Экзамен. Базовый синтаксис. (Исправления)

--- chapter_001/src/main/java/ru/job4j/array/ArrayMerge.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayMerge.java
@@ -9,49 +9,46 @@ public class ArrayMerge {
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         
-        int idxR = 0;
-        int idxL = 0;      
+        int idxL = 0;
+        int idxR = 0;              
         int idxRsl = 0;      
         
         int valL = 0;
-        int valR = 0;
+        int valR = 0;        
+        int valCur = 0;        
             
-        while (idxRsl < rsl.length) {           
+        while (idxRsl < rsl.length) {            
             
-            
-            if (idxL < left.length) {
-                valL = left[idxL];  
-            }
-                        
-            if (idxR < right.length) {
-                valR = right[idxR];      
-            }
-            
-            while ((valR < valL || idxL == left.length) && idxR < right.length) {
-                
-                if (idxR < right.length) {
-                    valR = right[idxR];      
-                }
-                
-                if (idxRsl < rsl.length) {
-                        rsl[idxRsl] = valR;
-                        idxRsl++;
-                }
-                
-                idxR++;           
-            }
-            
-            if (idxL < left.length) {
-                idxL++;
-            } else {
-                break;
-            }
-            
-            if (idxRsl < rsl.length) {
-                rsl[idxRsl] = valL;
-                idxRsl++;
+            if (idxL < left.length || idxR > right.length) { 
+                valL = left[idxL];                
             }            
             
+            if (idxR < right.length || idxL > left.length) {
+                valR = right[idxR];               
+            }
+            
+            if (idxL < left.length && idxR < right.length) {
+                if (valL <= valR) {
+                    valCur = valL;
+                    idxL++;
+                } else if (valL > valR) {
+                    valCur = valR;
+                    idxR++;
+                }
+            } else {
+                if (idxL >= left.length) {
+                    valCur = valR;
+                    idxR++;
+                } else if (idxR >= right.length) {
+                    valCur = valL;
+                    idxL++;
+                }
+            }            
+            
+            if (idxRsl < rsl.length) {
+                rsl[idxRsl] = valCur;
+                idxRsl++;
+            }           
         }       
         
         return rsl;

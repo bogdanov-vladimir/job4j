5671eacc6b54bd3caec1ef7f78a842534a2a964a
bogdanov-vladimir <bogd_vlad@mail.ru>
2019-12-11 19:13:45 +0500

    1.Создание объекта

---
+++ Oop/src/main/java/ru/job4j/Cat.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static main (String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}

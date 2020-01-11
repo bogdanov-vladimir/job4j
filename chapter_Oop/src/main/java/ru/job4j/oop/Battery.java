f9ac3651481ddda2fb7140890f0b8d3aa280eb2b
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-01-11 15:04:02 +0500

    1.7 Состояние объекта

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+/** 1.7. Состояние объекта.
+ *
+ * 1. Создать класс ru.job4j.oop.Battery. в нем должно быть поле private int load.
+ * 2. Создать метод public void exchange(Battery another). Этот метод должен списывать заряд из
+ * батареи у кого вызывали метод exchange и добавить к объекту another.
+ */
+
+public class Battery {
+    private int load;
+
+    public Battery(int iLoad) {
+        this.load = iLoad;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+    }
+
+    public static void main(String[] args) {
+        Battery bOne = new Battery(10);
+        Battery bTwo = new Battery(100);
+
+        System.out.println("bOne.load ="+ bOne.load +", bTwo.load ="+ bTwo.load);
+        bOne.exchange(bTwo);
+        System.out.println("bOne.load ="+ bOne.load +", bTwo.load ="+ bTwo.load);
+    }
+}

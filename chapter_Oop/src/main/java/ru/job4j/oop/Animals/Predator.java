5a208d87ff5f13a90a64dd494d65d28785f590cc
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-01-14 14:56:36 +0500

    2.Наследование : Конструктор по умолчанию

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Animals/Predator.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop.Animals;
+
+public class Predator extends Animal {
+    public Predator() {
+        super();
+        System.out.println("I`m Predator!");
+    }
+}
---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Animals/Story.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop.Animals;
+
+public class Story {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Predator predator = new Predator();
+        Tiger tiger = new Tiger();
+    }
+}

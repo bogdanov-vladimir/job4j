cc5896154e985fb02d661702a728cf9dedf9f2da
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-01-16 15:05:56 +0500

    3. Инкапсуляция

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Encapsulation/Config.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop.Encapsulation;
+
+public class Config {
+    private String name;
+    private int positions;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println(positions);
+    }
+
+    public String getProperty(String key) {
+        return searchKey(key);
+    }
+
+    private String searchKey(String key) {
+        return  key;
+    }
+
+}

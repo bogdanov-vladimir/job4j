22e5f153bea8f801d1730a73ea961a24ed5442d2
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-01-16 15:47:57 +0500

    3.Инкапсуляция : Модель данных

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Encapsulation/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop.Encapsulation.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setFio("Иванов Иван Иваныч");
+        student.setGroup("Прикладная математика");
+        student.setDateReceipt(new Date());
+
+        System.out.println("Студент: "+ student.getFio()
+                +", группа:"+ student.getGroup()
+                +", дата поступления: "+ student.getDateReceipt().toString());
+    }
+}

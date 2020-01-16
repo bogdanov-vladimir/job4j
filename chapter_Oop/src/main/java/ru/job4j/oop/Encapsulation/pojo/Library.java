52d12a4265e215e7514f3339f1b500f52f4049a4
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-01-16 16:25:53 +0500

    3. Инкапсуляция : Массивы и модели

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Encapsulation/pojo/Library.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop.Encapsulation.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+
+        for(int iBook = 0; iBook < books.length; iBook++) {
+            Book book = new Book("Clean code"+ (iBook == 0 ? "" : iBook), 130);
+            books[iBook] = book;
+
+            System.out.println(iBook +". "+ books[iBook].getName() +", Страниц: "+ books[iBook].getCountPages());
+        }
+
+        Book book0 = books[0];
+        Book book3 = books[2];
+        books[0] = book3;
+        books[2] = book0;
+
+        for(int iBook = 0; iBook < books.length; iBook++) {
+            if(books[iBook].getName().equals("Clean code")) {
+                System.out.println(books[iBook].getName() +", idx: "+ iBook);
+            }
+        }
+    }
+}

7f96f3c1c85fad4c72a2ed0f3c1047df5879af1a
bogdanov-vladimir <bogd_vlad@mail.ru>
2020-03-04 14:11:28 +0500

    3.3 Удаление моделей из массива

---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Encapsulation/pojo/Shop.java
@@ -0,0 +1,43 @@
+package ru.job4j.oop.Encapsulation.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Book[] books = new Book[5];
+
+        books[0] = new Book("Book0", 15);
+        books[1] = new Book("Book1", 5);
+        books[2] = null;
+        books[3] = null;
+        books[4] = new Book("Book4", 50);
+
+        for (int i = 0; i < books.length; i++) {
+            delete(books, i);
+        }
+
+
+        for(int i = 0; i < books.length; i++) {
+            if(books[i] == null) {
+                System.out.println("i = "+ i +" = null");
+            } else {
+                System.out.println("i = "+ i +" = "+ books[i].getName());
+            }
+        }
+    }
+
+    private static void delete(Book[] books, int index) {
+
+        if(books[index] == null) {
+            for(int j = 0; j < books.length; j++) {
+                int jNext = (books.length - 1) - j;
+
+                if(books[jNext] == null || jNext < index) {
+                    continue;
+                }
+
+                books[index] = books[jNext];
+                books[jNext] = null;
+                break;
+            }
+        }
+    }
+}
---
+++ chapter_Oop/src/main/java/ru/job4j/oop/Tracker/Tracker.java
@@ -0,0 +1,4 @@
+package ru.job4j.oop.Tracker;
+
+public class Tracker {
+}
